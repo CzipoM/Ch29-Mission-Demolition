5ce0b150ec218499fb081500be16fdeb014c121b
MJC504 <MJC504@CILABS-119A-09.scilsnet.rutgers.edu>
2017-09-29 19:09:29 -0400

    Created a trail off of the projectile, currently have a null pointer

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    static public Slingshot S;
     public GameObject launchPoint;
     public GameObject prefabProjectile;
     public float velocityMult = 4f;
@@ -12,6 +13,7 @@ public class Slingshot : MonoBehaviour {
     public bool aimingMode;
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
